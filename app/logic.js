043f8df40aef3bdca0f8f4b770805757e7f2c4c2
MiroslavVelikov <79000592+MiroslavVelikov@users.noreply.github.com>
2021-07-14 15:12:10 +0300

    Adding Modal box

--- app/logic.js
+++ app/logic.js
@@ -19,6 +19,27 @@ function login() {
     window.close();
 }
 
+function confirmPromp() {
+    let modal = document.getElementById("deleteModal");
+    let btn = document.getElementById("btnDelete");
+    let span = document.getElementsByClassName("close")[0];
+    
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+}
+
+function deleteUser() {
+
+}
 // function changeColor(user) {
 //     user.style.background='#000000'
 // }
